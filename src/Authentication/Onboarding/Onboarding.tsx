9766039851e7ba05a818c7ee4d42c965831714b5
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2020-12-04 00:00:46 +1100

    Added new project

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+interface ComponentNameProps {}
+
+const ComponentName = () => {
+return <View></View>
+};
+export default ComponentName;
